9a098acb36d481250c4ebb7e43f5f0da9be2303e
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2022-03-28 17:07:52 +0200

    Close #93: Add possibility to configure timeout (and some more properties) per worker.

--- micronaut-zeebe-client-feature/src/main/java/info/novatec/micronaut/zeebe/client/feature/ZeebeWorker.java
+++ micronaut-zeebe-client-feature/src/main/java/info/novatec/micronaut/zeebe/client/feature/ZeebeWorker.java
@@ -21,6 +21,7 @@ import io.micronaut.context.annotation.Parallel;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import java.time.Duration;
 
 /**
  * @author Martin Sawilla
@@ -34,6 +35,48 @@ import java.lang.annotation.Target;
 @Parallel
 public @interface ZeebeWorker {
 
+    /**
+     * Set the type of jobs to work on.
+     *
+     * @see io.camunda.zeebe.client.api.worker.JobWorkerBuilderStep1#jobType(String)
+     *
+     * @return the job type
+     */
     String type();
 
+    /**
+     * Set the time for how long a job is exclusively assigned for this worker, e.g "PT5M", see format definition in {@link java.time.Duration#parse(CharSequence)}
+     *
+     * See also {@link io.camunda.zeebe.client.api.worker.JobWorkerBuilderStep1.JobWorkerBuilderStep3#timeout(Duration)}
+     *
+     * @return the job timeout
+     */
+    String timeout() default "";
+
+    /**
+     * Set the maximum number of jobs which will be exclusively activated for this worker at the same time.
+     *
+     * See also {@link io.camunda.zeebe.client.api.worker.JobWorkerBuilderStep1.JobWorkerBuilderStep3#maxJobsActive(int)}}
+     *
+     * @return the maximum number of active jobs
+     */
+    int maxJobsActive() default -1;
+
+    /**
+     * Set the request timeout for activate job request used to poll for new job, e.g "PT20S", see format definition in {@link java.time.Duration#parse(CharSequence)}
+     *
+     * See also {@link io.camunda.zeebe.client.api.worker.JobWorkerBuilderStep1.JobWorkerBuilderStep3#requestTimeout(Duration)}
+     *
+     * @return the request timeout
+     */
+    String requestTimeout() default "";
+
+    /**
+     * Set the maximal interval between polling for new jobs, e.g "PT0.1S" for 100ms, see format definition in {@link java.time.Duration#parse(CharSequence)}
+     *
+     * See also {@link io.camunda.zeebe.client.api.worker.JobWorkerBuilderStep1.JobWorkerBuilderStep3#pollInterval(Duration)}
+     *
+     * @return poll interval
+     */
+    String pollInterval() default "";
 }
