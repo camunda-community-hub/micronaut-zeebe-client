7d8c5da29743cb8f7db62e6ed81be800aaba43ee
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2022-05-06 10:59:06 +0200

    Update to Zeebe 8.0.1

--- micronaut-zeebe-client-example/src/test/java/info/novatec/micronaut/zeebe/client/example/TestcontainersIntegrationTest.java
+++ micronaut-zeebe-client-example/src/test/java/info/novatec/micronaut/zeebe/client/example/TestcontainersIntegrationTest.java
@@ -19,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 class TestcontainersIntegrationTest {
 
     @Container
-    ZeebeContainer zeebeContainer = new ZeebeContainer(DockerImageName.parse("camunda/zeebe:8.0.0"));
+    ZeebeContainer zeebeContainer = new ZeebeContainer(DockerImageName.parse("camunda/zeebe:8.0.1"));
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
