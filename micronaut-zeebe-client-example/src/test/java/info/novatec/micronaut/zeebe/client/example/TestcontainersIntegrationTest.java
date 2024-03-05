9bbe93b7a8ab42a5108f19f4b70d39ddb7949c3c
Silvio Wangler <71073+saw303@users.noreply.github.com>
2024-03-05 19:00:03 +0100

    Upgrade to Zeebe 8.4.4

--- micronaut-zeebe-client-example/src/test/java/info/novatec/micronaut/zeebe/client/example/TestcontainersIntegrationTest.java
+++ micronaut-zeebe-client-example/src/test/java/info/novatec/micronaut/zeebe/client/example/TestcontainersIntegrationTest.java
@@ -19,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 class TestcontainersIntegrationTest {
 
     @Container
-    ZeebeContainer zeebeContainer = new ZeebeContainer(DockerImageName.parse("camunda/zeebe:8.3.4"));
+    ZeebeContainer zeebeContainer = new ZeebeContainer(DockerImageName.parse("camunda/zeebe:8.4.4"));
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
